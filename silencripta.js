b89ce76681a1e58f0abfdc9451ddd2390ba4c0ee
Silvana Barea <110042826+gitdesil@users.noreply.github.com>
2024-08-27 22:57:53 -0300

    primera version de encriptador

---
+++ silencripta.js
@@ -0,0 +1,35 @@
+function encriptarTexto(texto) {
+    return texto
+        .replace(/e/g, 'enter')
+        .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
+        .replace(/o/g, 'ober')
+        .replace(/u/g, 'ufat');
+}
+
+function desencriptarTexto(texto) {
+    return texto
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+}
+
+document.getElementById('encriptar').addEventListener('click', function() {
+    const texto = document.getElementById('texto').value;
+    const textoEncriptado = encriptarTexto(texto);
+    document.getElementById('resultado').value = textoEncriptado;
+});
+
+document.getElementById('desencriptar').addEventListener('click', function() {
+    const texto = document.getElementById('texto').value;
+    const textoDesencriptado = desencriptarTexto(texto);
+    document.getElementById('resultado').value = textoDesencriptado;
+});
+
+document.getElementById('copiar').addEventListener('click', function() {
+    const resultado = document.getElementById('resultado');
+    resultado.select();
+    document.execCommand('copy');
+});
